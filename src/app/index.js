e7e68497125885bab80275f250158ffe99d6ddb6
Jonas <jonasperezmira@gmail.com>
2020-11-25 15:38:40 +0100

    Slider continuo funcionando y el manual comentado, y la notificacion para las cookies funcionando (una vez aceptas se guarda la cookie, y no aparece más, para que salga borrar la cookie de las cookies del almaceniamiento de navegador)

--- src/app/index.js
+++ src/app/index.js
@@ -1,8 +1,9 @@
 import './css/style.scss';
 import jedliSlider from 'jedlislider/src-webpack/jedlislider.js';
+import cookieNoticeJS from "cookie-notice/dist/cookie.notice.min.js";
 import 'jedlislider/src-webpack/jedlislider.scss';
 
-let sliderMain = new jedliSlider(document.querySelector("[data-item='slider-main']"), {
+const sliderMain = new jedliSlider(document.querySelector("[data-item='slider-main']"), {
     "mode": "continuous",
     "slidesWidth": "equal",
     "visibleSlides": "2",
@@ -10,6 +11,31 @@ let sliderMain = new jedliSlider(document.querySelector("[data-item='slider-main
     "direction": "left",
     "pauseOnHover": "true"
 })
+// //comentado el codigo para pasar manualmente los elementos del slider
+// const sliderMain = new jedliSlider(document.querySelector("[data-item='slider-main']"), {
+//     "mode": "default",
+//     "slidesWidth": "equal",
+//     "infinite": "true",
+//     "visibleSlides": 1,
+//     "easing": "ease-out",
+//     "slidesToScroll": 1,
+//     "speed": "600",
+//     "preventOverScroll": "false",
+//     "animationChange": "fade"
+
+// })
 document.querySelector("sliderMain").addEventListener('beforeInit', () => {
+    console.log("beforeInit");
+})
+// document.querySelector("[data-action='slider-prev']").addEventListener("click", () => {
+//     sliderCustomEasing.slidePrev();
+// })
+// document.querySelector("[data-action='slider-next']").addEventListener("click", () => {
+//     sliderCustomEasing.slideNext();
+// })
+
+const cookies = new cookieNoticeJS();
+
+document.querySelector("cookies").addEventListener('beforeInit', () => {
     console.log("beforeInit");
 })
