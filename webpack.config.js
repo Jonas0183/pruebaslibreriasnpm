390b23db1c103439a0545475e3ef61513e528f93
Jonas <jonasperezmira@gmail.com>
2020-11-25 09:20:30 +0100

    Primer commit con estructura de directorios y webpack configurado, ademas de creacion de archivo de estilos y un index.html e index.js

---
+++ webpack.config.js
@@ -0,0 +1,101 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry : {
+            app : [
+                './src/app/index.js',
+                "@babel/polyfill/noConflict"
+            ]
+    },
+    output : {
+        path : path.resolve(__dirname, 'dist'),
+        filename : 'js/app.bundle.js'
+    },
+    devtool : 'source-map',
+    module : {
+        rules : [
+            {
+                test :  /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.(gif|png|jpe?g|sgv)$/i,
+                exclude: /node_modules/,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options: {
+                      name: '[name].[ext]',
+                      outputPath: 'assets/images',
+                      publicPath: 'assets/images'
+                    }
+                  }
+                ]                  
+              },
+            {                       
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.html$/,
+                exclude: /node_modules/,
+                use: "html-loader"
+            }
+        ]
+    },
+
+    plugins : [
+        new MiniCssExtractPlugin({
+            filename: 'css/style.scss',
+          }),
+
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            minify : {
+                collapseWhitespace: true,
+                removeComments: true,
+                removeRedundantAttributes: true,
+                removeScriptTypeAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                useShortDoctype: true
+            }
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/addNew.html',
+            filename : 'addNew.html',
+            minify : {
+                collapseWhitespace: true,
+                removeComments: true,
+                removeRedundantAttributes: true,
+                removeScriptTypeAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                useShortDoctype: true
+            }
+        }),
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+      }
+};
